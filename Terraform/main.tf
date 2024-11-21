ffd4632067ecfa7dbcf85bed9605ac13436beffa
Pete Leese <pete.leese@hashicorp.com>
2024-11-21 22:55:46 +0000

    Additional resources mapped

---
+++ Terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    ukumawapi = {
+      source  = "pete-leese/ukumawapi"
+      version = "0.4.5"
+    }
+  }
+}
+
+provider "ukumawapi" {
+  # Configuration options
+}
